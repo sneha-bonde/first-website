54c3f1ed29ebccb74ff7391bdaa51085c5a7985b
sneha-bonde <120788924+sneha-bonde@users.noreply.github.com>
2024-02-20 21:23:38 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,5 @@
+function chengeWidth(){
+    var search_input =document.querySelector('.text_width');
+    // var search_input = document.querySelector('.')
+    search_input.style.width ="250px";
+}
